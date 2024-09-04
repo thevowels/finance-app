ea40428378e8315355dc91aa5b67c4bad33c7471
thevowels <aungphyohtet1@gmail.com>
2024-09-05 03:01:10 +0630

    Type casting from string to number all I'm doing is now fixing the version mismatch.

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -109,7 +109,7 @@ const app = new Hono()
                         and(
                             accountId ? eq(transactions.accountId, accountId) : undefined,
                             eq(accounts.userId, auth.userId),
-                            lt(transactions.amount, 0),
+                            lt(sql`${transactions.amount}::numeric`, 0),
                             gte(transactions.date, startDate),
                             lte(transactions.date, endDate)
                         )
