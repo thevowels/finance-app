a7ac2902fa6d868f94fa577324706b7be3df67b8
thevowels <aungphyohtet1@gmail.com>
2024-09-09 21:28:28 +0630

    updating layout

--- app/(dashboard)/categories/page.tsx
+++ app/(dashboard)/categories/page.tsx
@@ -4,6 +4,22 @@ import {Metadata} from "next";
 export const metadata: Metadata={
     title: "Categories",
     description:" Categories for You!",
+    openGraph: {
+        title: 'Yours Finance',
+        description: 'Track your income & expenses with ease using our app.',
+        url: 'https://shadcn-finance.vercel.app',
+        siteName: 'Shadcn Finance',
+        type: 'website',
+        images: [
+            {
+                url: 'https://www.example.com/shadcn-logo.png',
+                width: 800,
+                height: 600,
+                alt: 'Shadcn Finance Logo',
+            },
+        ],
+    },
+
 }
 export default function Page(){
     return(
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,28 @@
 import Header from "@/components/Header";
+import {Metadata} from "next";
 
 type Props = {
     children: React.ReactNode;
+}
+export const metadata: Metadata={
+    title: "Shadcn Finance",
+    description:" 'Track your income & expenses with ease using our app.!",
+    openGraph: {
+        title: 'Yours Finance',
+        description: 'Track your income & expenses with ease using our app.',
+        url: 'https://shadcn-finance.vercel.app',
+        siteName: 'Shadcn Finance',
+        type: 'website',
+        images: [
+            {
+                url: 'https://www.example.com/shadcn-logo.png',
+                width: 800,
+                height: 600,
+                alt: 'Shadcn Finance Logo',
+            },
+        ],
+    },
+
 }
 
 const DashboardLayout = ({children}: Props) =>{
