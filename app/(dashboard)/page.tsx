63431e9bda52463c3ba8da0bc798e16c4e54558e
thevowels <aungphyohtet1@gmail.com>
2024-09-09 21:36:10 +0630

    adding metadata on root page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,26 @@
 import DataGrid from "@/components/data-grid";
 import {DataCharts} from "@/components/data-chart";
+import {Metadata} from "next";
+export const metadata: Metadata={
+    title: "Shadcn Finance",
+    description:" 'Track your income & expenses with ease using our app.!",
+    openGraph: {
+        title: 'Yours Finance',
+        description: 'Track your income & expenses with ease using our app.',
+        url: 'https://shadcn-finance.vercel.app',
+        siteName: 'Shadcn Finance',
+        type: 'website',
+        images: [
+            {
+                url: 'https://www.example.com/shadcn-logo.png',
+                width: 800,
+                height: 600,
+                alt: 'Shadcn Finance Logo',
+            },
+        ],
+    },
 
+}
 
 export default function Home() {
 
